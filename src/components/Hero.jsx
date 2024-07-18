78ce2911bd93aa116a367d32a7acfaa7f5987617
Ayshakhan01 <t.ayshakhanam@gmail.com>
2024-07-18 11:56:17 +0530

    ak_commmit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,93 @@
+import React, { useRef, useState } from 'react';
+import { TypeAnimation } from 'react-type-animation';
+import { Row, Col } from 'react-bootstrap';
+import image from '../images/girlno.jpg';
+import hi from '../images/hi.png';
+
+export default function Hero() {
+    const [isDragging, setIsDragging] = useState(false);
+    const [initialPosition, setInitialPosition] = useState({ x: 0, y: 0 });
+    const [offset, setOffset] = useState({ x: 0, y: 0 });
+    const imageRef = useRef(null);
+
+    const handleMouseDown = (e) => {
+        setIsDragging(true);
+        const imageRect = imageRef.current.getBoundingClientRect();
+        setOffset({
+            x: e.clientX - imageRect.left,
+            y: e.clientY - imageRect.top
+        });
+        setInitialPosition({
+            x: imageRect.left,
+            y: imageRect.top
+        });
+        imageRef.current.style.cursor = 'grabbing';
+    };
+
+    const handleMouseMove = (e) => {
+        if (isDragging) {
+            const x = e.clientX - offset.x;
+            const y = e.clientY - offset.y;
+            imageRef.current.style.transform = `translate(${x}px, ${y}px)`;
+        }
+    };
+
+    const handleMouseUp = () => {
+        if (isDragging) {
+            setIsDragging(false);
+            imageRef.current.style.cursor = 'grab';
+            imageRef.current.style.transform = 'translate(0px, 0px)';
+        }
+    };
+
+    const handleMouseLeave = () => {
+        if (isDragging) {
+            setIsDragging(false);
+            imageRef.current.style.cursor = 'grab';
+            imageRef.current.style.transform = 'translate(0px, 0px)';
+        }
+    };
+
+    return (
+        <div className='hero' id='hero'>
+            <div className='container' style={{ paddingTop: '110px' }}>
+                <Row className="d-flex flex-column-reverse flex-md-row">
+                    <Col sm={12} md={8}>
+                        <div className='text'>
+                            <h5 style={{ display:'flex',alignItems:'center',color: 'white', fontWeight: '600' }}>Hey Devs <img src={hi} style={{margin:'0px 5px'}} width='30' height='30' alt="hi" /> ,</h5>
+                            <TypeAnimation
+                                sequence={[
+                                    'I am Aysha Khan', 1500,
+                                    'I am a Developer', 1500,
+                                    'I am a Hacker', 1500,
+                                    'I am a Designer', 1500,
+                                    'I am a Programmer', 1500,
+                                ]}
+                                wrapper="span"
+                                speed={50}
+                                style={{ color: '#89CFF0', fontSize: '2em', display: 'inline-block' }}
+                                repeat={Infinity}
+                            /><br /><br />
+                            <p style={{ color: 'white', fontWeight: '600' }}>
+                            I am enthusiastic about creating high-quality web applications with the <span style={{ color: '#89CFF0' }}>MERN Stack</span>and have a strong focus on <span style={{ color: '#89CFF0' }}>Cyber Security</span>. I am committed to delivering smooth user experiences through clean and efficient code. Let's work together to bring your ideas to life! <br /><br /></p>
+                            <a className='resume-btn' rel='noreferrer' target='_blank' href="https://drive.google.com/file/d/1BPPpFdCxA2ySajQWQqQfCkNCfU8z4v29/view?usp=sharing">Resume</a>
+                            <br /><br />
+                        </div>
+                    </Col>
+                    <Col sm={12} md={4} className="d-flex justify-content-center">
+                        <img
+                            ref={imageRef}
+                            className='img animated-image'
+                            src={image}
+                            alt="Not found"
+                            onMouseDown={handleMouseDown}
+                            onMouseMove={handleMouseMove}
+                            onMouseUp={handleMouseUp}
+                            onMouseLeave={handleMouseLeave}
+                        />
+                    </Col>
+                </Row>
+            </div>
+        </div>
+    );
+}
---
+++ src/utils/constants.js
@@ -0,0 +1,51 @@
+
+import zomato from '../images/zomatoclone.png'
+import firewall from '../images/firewallsimulator.png'
+import sales from '../images/salesdashboard.png'
+import snake from '../images/snakegame.png'
+import tech_tools from '../images/tech_tools.png'
+
+export const projects = [
+    {
+        title: 'Zomato_Clone',
+        techstack: ['React', 'Material UI', 'BootStrap'],
+        description: 'The Zomato frontend clone is a smooth food delivery platform developed with React Hooks and Shimmer UI to provide an improved user experience (UX).',
+        github_url: 'https://github.com/Ayshakhan01/Zomato_clone',
+        // live_demo: 'https://splashbysurya.netlify.app/',
+        img: zomato,
+    },
+    {
+        title: 'Firewall Simulator',
+        techstack: ['Python','PyQt5','Prophet'],
+        description: 'I created a basic firewall simulator for Ubuntu with a GUI that allows users to manually block or unblock IP addresses based on their activity.',
+        github_url: 'https://github.com/Ayshakhan01/Basic-Firewall-Simulator-With-Malicious-Bot-Detecton',
+        // live_demo: 'https://loveanalyzer.netlify.app/',
+        img:firewall
+    },
+    
+    {
+        title: 'Sales Dashboard',
+        techstack: ['Looker Studio'],
+        description: 'I have developed a comprehensive sales dashboard that facilitates detailed analysis of sales performance based on the provided data.',
+        github_url: 'https://github.com/Ayshakhan01/Sales_Dashboard_W1_STI',
+        // live_demo: 'https://weatherhub-528.netlify.app/',
+        img:sales
+    },
+    {
+        title: 'Tech Tools',
+        techstack: ['React', 'Material UI', 'BootStrap'],
+        description: 'Tech Tools is a platform which offers softwares for Engineering students. Styled using BootStrap and Material UI for better user experience ',
+        github_url: 'https://github.com/Ayshakhan01/Tech-Tools/tree/main/Tech-Tools-main',
+        // live_demo: 'https://techtoolsbysurya.netlify.app/',
+        img:tech_tools
+    },
+    {
+        title: 'Snake-Game',
+        techstack: ['React', 'HTML', 'Material UI','CSS'],
+        description: 'Created a web-based Snake game featuring essential functionalities such as food consumption and score incrementation.',
+        github_url: 'https://github.com/Ayshakhan01/react_snake_game',
+        // live_demo: 'https://splashbysurya.netlify.app/',
+        img:snake
+    },
+
+]
